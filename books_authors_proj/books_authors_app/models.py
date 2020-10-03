47a5c760e98b5cba1d67d598877479ee659237b5
dylanPMurphy <dylanmurphydba@gmail.com>
2020-10-03 15:41:06 -0700

    changed name of 4th author to bill

--- books_authors_proj/books_authors_app/models.py
+++ books_authors_proj/books_authors_app/models.py
@@ -10,6 +10,7 @@ class Book(models.Model):
 class Author(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    notes = models.TextField()
     books = models.ManyToManyField(Book, related_name="authors")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
