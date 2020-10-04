a8e78e9a2e2a07d15dc50016e63990f7a9336e20
dylanPMurphy <dylanmurphydba@gmail.com>
2020-10-04 16:46:42 -0700

    added templates

--- books_authors_proj/books_authors_app/views.py
+++ books_authors_proj/books_authors_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from books_authors_app.models import *
 # Create your views here.
+def index(request):
+    context ={
+        'allBooks': Books.objects.all()
+    }
+    return render(request, index.html, context)
